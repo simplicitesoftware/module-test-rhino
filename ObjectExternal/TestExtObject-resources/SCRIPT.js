48c92c2e43908a41a792d6a8bf4d6eb14addb050
designer (Designer) <>
2018-06-18 00:11:47 +0200

    Updated

--- ObjectExternal/TestExtObject-resources/SCRIPT.js
+++ ObjectExternal/TestExtObject-resources/SCRIPT.js
@@ -1,12 +1,12 @@
-TestExtObject = (function($) {
+if (typeof TestExtObject === "undefined") TestExtObject = (function($) {
 	var app;
-	
-	function ready(root) {
+
+	function render(root) {
 		app = new Simplicite.Ajax(root, "ui");
 		app.getGrant(function() {
 			$("#test").html("Hello " + app.grant.login + "!");
 		});
 	}
-	
-	return { ready: ready };
+
+	return { render: render };
 })(jQuery);
--- ObjectExternal/TestExtObject.js
+++ ObjectExternal/TestExtObject.js
@@ -1,14 +1,35 @@
 TestExtObject.display = function(params) {
-	this.setDecoration(false);
-	var root = params.getRoot();
-	var wp = new JQueryWebPage(root, this.getDisplay());
-	wp.appendAjax();
-	wp.appendJSInclude(HTMLPage.getResourceJSURL(this, "SCRIPT"));
-	wp.setReady(this.getName() + ".ready(\"" + root + "\");");
-	wp.append("<div id=\"test\"></div>");
-	return wp.toString();
-	/*
-	var o = Grant.getSystemAdmin().getTmpObject("DemoClient");
-	return o.getLabel();
-	*/
+	var type = params.getParameter(type);
+	if (type == "pdf") {
+		this.setMIMEType(HTTPTool.MIME_TYPE_PDF); // Must force the MIME type
+		importPackage(Packages.com.lowagie.text);
+		var bos = new java.io.ByteArrayOutputStream();
+		var pdf = PDFTool.open(bos);
+		pdf.add(new Phrase("Hello world !"));
+		PDFTool.close(pdf);
+		return bos.toByteArray();
+	} else if (type == "xls") {
+		this.setMIMEType(HTTPTool.MIME_TYPE_XLS); // Must force the MIME type
+		var xls = new ExcelPOITool();
+		var s = xls.newSheet("MySheet");
+		var r = xls.newRow(0);
+		r.add(xls.newCell(0, "Hello"));
+		r.add(xls.newCell(1, "world !"));
+		s.add(r);
+		xls.add(s);
+		return xls.generateToByteArray();
+	} else if (type == "json") {
+		this.setMIMEType(HTTPTool.MIME_TYPE_JSON); // Must force the MIME type
+		return new JSONObject().put("hello", "world");
+	} else {
+		this.setDecoration(false);
+		var root = params.getRoot();
+		var wp = new JQueryWebPage(root, this.getDisplay());
+		wp.appendAjax();
+		wp.appendCSSInclude(HTMLPage.getResourceCSSURL(this, "STYLES"));
+		wp.appendJSInclude(HTMLPage.getResourceJSURL(this, "SCRIPT"));
+		wp.setReady(this.getName() + ".render(\"" + root + "\");");
+		wp.append("<div id=\"test\"></div>");
+		return wp.toString();
+	}
 };
